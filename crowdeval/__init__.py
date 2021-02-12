80900d89666a80d6b3d4c157365126142a02371e
Freddy Heppell <freddy@freddyheppell.com>
2021-02-12 15:23:26 +0000

    fix linting errors

--- crowdeval/__init__.py
+++ crowdeval/__init__.py
@@ -1,1 +1,3 @@
+"""Main package of the CrowdEval app."""
+
 __version__ = "0.1.0"
--- crowdeval/test/views.py
+++ crowdeval/test/views.py
@@ -8,4 +8,4 @@ blueprint = Blueprint("public", __name__, static_folder="../static")
 @blueprint.route("/")
 def index():
     """Test route."""
-    return render_template('index.html')
+    return render_template("index.html")
