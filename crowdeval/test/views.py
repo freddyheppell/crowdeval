b6b3cafc1cbf9d23427e5ddad000fc5124ef36af
Freddy Heppell <freddy@freddyheppell.com>
2021-02-12 14:05:18 +0000

    Reconfigure py linter

--- crowdeval/test/views.py
+++ crowdeval/test/views.py
@@ -1,18 +1,11 @@
 """Public section, including homepage and signup."""
 
-from flask import (
-    Blueprint,
-    current_app,
-    flash,
-    redirect,
-    render_template,
-    request,
-    url_for,
-)
+from flask import Blueprint
 
 blueprint = Blueprint("public", __name__, static_folder="../static")
 
 
 @blueprint.route("/")
 def index():
+    """Test route."""
     return "hey"
