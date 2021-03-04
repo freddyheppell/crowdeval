c2845cc8a47ad9489b84e597b71182b4ec96a6a5
Freddy Heppell <freddy@freddyheppell.com>
2021-03-04 16:13:33 +0000

    Run pytest in subprocess

--- crowdeval/commands.py
+++ crowdeval/commands.py
@@ -8,15 +8,13 @@ import click
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.join(HERE, os.pardir)
-TEST_PATH = os.path.join(PROJECT_ROOT, "tests")
+TEST_PATH = "tests/"
 
 
 @click.command()
 def test():
     """Run the tests."""
-    import pytest
-
-    rv = pytest.main([TEST_PATH, "--verbose"])
+    rv = call(["pytest", TEST_PATH, '--verbose'])
     exit(rv)
 
 
