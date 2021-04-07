aa025edf889c78b97ad844a16f4f27f3f4075d3e
Freddy Heppell <freddy@freddyheppell.com>
2021-04-07 22:29:06 +0100

    Finish generator for dummy posts

--- crowdeval/posts/support/dummy_post.py
+++ crowdeval/posts/support/dummy_post.py
@@ -1,5 +1,9 @@
 """An example social network implementation."""
 
+import json
+
+from faker import Faker
+from faker.providers import date_time, lorem, profile, python
 
 from crowdeval.posts.support.platform import Platform
 
@@ -18,3 +22,26 @@ class DummyPost:
     def get_external_id(self):
         """Get the retrieved status id."""
         return self.id
+
+    def get_data(self):
+        faker = Faker()
+        Faker.seed(self.id)
+        faker.add_provider(lorem)
+        faker.add_provider(profile)
+        faker.add_provider(python)
+        faker.add_provider(date_time)
+        author = faker.simple_profile()
+
+        return {
+            "platform": self.get_platform().value,
+            "external_post_id": self.id,
+            "text": faker.paragraph(),
+            "author_name": author["name"],
+            "author_username": author["username"],
+            "author_profile_url": "https://picsum.photos/seed/{id}/256/256".format(
+                id=self.id
+            ),
+            "external_author_id": faker.pyint(),
+            "additional_metadata": json.dumps({"verified": faker.pybool()}),
+            "external_created_at": faker.date_time_this_century(before_now=True).strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+        }
