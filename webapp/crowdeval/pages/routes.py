d5eaa3d33b6e6eb987d79d34d01f4e767dca103d
Freddy Heppell <freddy@freddyheppell.com>
2021-07-02 14:02:04 +0100

    Add blank terms and privacy page

--- webapp/crowdeval/pages/routes.py
+++ webapp/crowdeval/pages/routes.py
@@ -9,3 +9,15 @@ blueprint = Blueprint("pages", __name__, static_folder="../static")
 def index():
     """Index page route."""
     return render_template("index.html")
+
+
+@blueprint.route("/terms")
+def terms():
+    """Terms and conditions page."""
+    return render_template("pages/terms.html")
+
+
+@blueprint.route("/privacy")
+def privacy():
+    """Privacy page."""
+    return render_template("pages/privacy.html")
